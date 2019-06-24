25ae5361600ab9ac41221bd441cd797bcdc626c9
Xavier Clerc <xclerc@janestreet.com>
2019-06-24 09:33:47 +0100

    v0.13-preview.120.41+204

--- src/fifo.ml
+++ src/fifo.ml
@@ -1,4 +1,5 @@
 open! Import
+open Signal
 
 
 type t =
@@ -10,25 +11,69 @@ type t =
   ; used         : Signal.t }
 [@@deriving sexp_of]
 
+type create_fifo
+  =  ?nearly_empty    : int       (** default is [1] **)
+  -> ?nearly_full     : int       (** default is [depth-1] **)
+  -> ?overflow_check  : bool      (** default is [true] *)
+  -> ?reset           : Signal.t  (** default is [empty] **)
+  -> ?underflow_check : bool      (** default is [true] *)
+  -> unit
+  -> capacity : int
+  -> clock : Signal.t
+  -> clear : Signal.t
+  -> wr    : Signal.t
+  -> d     : Signal.t
+  -> rd    : Signal.t
+  -> t
+
+(* Generates wbr memory with explicit collision detection to gurantee [wbr] behaviour.
+   Despite what's suggested by Vivado's BRAM documentation, [write_first] are not
+   respected, even in SDP RAM mode.
+*)
+let ram_wbr_safe capacity ~write_port ~read_port =
+  let open Signal in
+  let collision =
+    (reg
+       (Reg_spec.create ~clock:write_port.write_clock ())
+       ~enable:read_port.read_enable
+       (write_port.write_enable
+        &: read_port.read_enable
+        &: (write_port.write_address ==: read_port.read_address)))
+  in
+  mux2 collision
+    (reg
+       (Reg_spec.create ~clock:write_port.write_clock ())
+       ~enable:(read_port.read_enable)
+       write_port.write_data)
+    (ram_rbw capacity ~write_port ~read_port)
+
+;;
+
 let create
+      ?(showahead = false)
       ?(nearly_empty = 1)
       ?(nearly_full)
       ?(overflow_check = true)
       ?(reset = Signal.empty)
-      ?(showahead = false)
       ?(underflow_check = true)
       ()
-      ~capacity ~clock ~clear ~wr ~d ~rd =
-  let open Signal in
+      ~capacity:ram_capacity ~clock ~clear ~wr ~d ~rd =
   if Signal.is_empty clear && Signal.is_empty reset
   then raise_s [%message
          "[Fifo.create] requires either a synchronous clear or asynchronous reset"];
   let reg_spec = Reg_spec.create ~clock ~clear ~reset () in
   let reg ?clear_to ~enable d = reg (Reg_spec.override reg_spec ?clear_to) ~enable d in
-  let abits = address_bits_for capacity in
-  let ubits = num_bits_to_represent capacity in
+  let abits = address_bits_for ram_capacity in
+  let actual_capacity =
+    (* to be consistent with Vivado's FIFO implementation, when instantiating a fwft FIFO,
+       it's actual capacity is added by one due to the additional register in the prefetch
+       buffer register.
+    *)
+    if showahead then ram_capacity + 1 else ram_capacity
+  in
+  let ubits = num_bits_to_represent actual_capacity in
   (* get nearly full/empty levels *)
-  let nearly_full = match nearly_full with None -> capacity-1 | Some x -> x in
+  let nearly_full = match nearly_full with None -> actual_capacity-1 | Some x -> x in
   let empty, full = wire 1, wire 1 in
   (* safe rd/wr signals assuming fifo neither full or empty *)
   let rd = if underflow_check then (rd &: ~: empty) -- "RD_INT" else rd in
@@ -45,35 +90,44 @@ let create
   used <== reg ~enable (used_next -- "USED_NEXT");
   (* full empty flags *)
   empty <== reg ~enable ~clear_to:vdd (used_next ==:. 0);
-  full <== reg ~enable (used_next ==:. capacity);
+  full <== reg ~enable (used_next ==:. actual_capacity);
   (* nearly full/empty flags *)
   let nearly_empty = reg ~enable ~clear_to:vdd (used_next <:. nearly_empty) in
   let nearly_full = reg ~enable (used_next >=:. nearly_full) in
   (* read/write addresses within fifo *)
   let addr_count enable name =
     let a = wire abits in
-    let an = mod_counter ~max:(capacity-1) a in
+    let an = mod_counter ~max:(ram_capacity-1) a in
     a <== reg ~enable an;
     a -- name, an -- (name ^ "_NEXT")
   in
-  let ra, ra_n = addr_count rd "READ_ADDRESS" in
-  let wa, _ = addr_count wr "WRITE_ADDRESS" in
-  (* fifo memory *)
   let q =
     if showahead
     then
-      let re = (rd |: (wr &: empty)) -- "RE" in
-      let ra = (mux2 rd ra_n ra) -- "RA" in
-      ram_wbr capacity
-        ~write_port:{ write_clock = clock
-                    ; write_enable = wr
-                    ; write_address = wa
-                    ; write_data = d }
-        ~read_port:{ read_clock = clock
-                   ; read_enable = re
-                   ; read_address = ra }
+      let used_is_one = reg ~enable:(rd ^: wr) (used_next ==:. 1) in
+      let used_gt_one = reg ~enable:(rd ^: wr) (used_next >:. 1) in
+      let memory =
+        let wr = wr &: (used_gt_one |: (used_is_one &: ~:rd)) in
+        let rd = rd &: used_gt_one in
+        let ra, ra_n = addr_count rd "READ_ADDRESS" in
+        let ra = mux2 rd ra_n ra -- "RA" in
+        let wa, _ = addr_count wr "WRITE_ADDRESS" in
+        ram_wbr_safe ram_capacity
+          ~write_port:{ write_clock = clock
+                      ; write_enable = wr
+                      ; write_address = wa
+                      ; write_data = d }
+          ~read_port:{ read_clock = clock
+                     ; read_enable = vdd
+                     ; read_address = ra }
+      in
+      let bypass_cond = ((empty &: wr) |: (used_is_one &: wr &: rd)) in
+      (mux2 bypass_cond d memory
+       |> reg ~enable:(bypass_cond |: rd))
     else
-      ram_rbw capacity
+      let ra, _ = addr_count rd "READ_ADDRESS" in
+      let wa, _ = addr_count wr "WRITE_ADDRESS" in
+      ram_rbw ram_capacity
         ~write_port:{ write_clock = clock
                     ; write_enable = wr
                     ; write_address = wa
@@ -88,3 +142,87 @@ let create
   ; nearly_full
   ; nearly_empty
   ; used }
+
+let create_classic_with_extra_reg
+      ?nearly_empty
+      ?nearly_full
+      ?overflow_check
+      ?reset
+      ?underflow_check
+      ()
+      ~capacity ~clock ~clear ~wr ~d ~rd =
+  let spec = Reg_spec.create ~clock ~clear () in
+  let fifo_valid = wire 1 in
+  let middle_valid = wire 1 in
+  let fifo_rd_en = wire 1 in
+  let empty = ~:(fifo_valid |: middle_valid) in
+  let will_update_dout = ~:empty &: rd in
+  let will_update_middle =
+    fifo_valid &: (middle_valid ==: will_update_dout)
+  in
+  let fifo =
+    create ~showahead:false ?nearly_empty ?nearly_full ?overflow_check ?reset ?underflow_check ()
+      ~capacity ~clock ~clear ~wr ~d ~rd:fifo_rd_en
+  in
+  let middle_dout = reg spec ~enable:will_update_middle fifo.q in
+  fifo_rd_en <== (~:(fifo.empty) &: ~:(middle_valid &: fifo_valid));
+  fifo_valid <==
+  reg spec ~enable:(fifo_rd_en |: will_update_middle |: will_update_dout) fifo_rd_en;
+  middle_valid <==
+  reg spec ~enable:(will_update_middle |: will_update_dout) will_update_middle;
+  { fifo with
+    q = reg spec ~enable:will_update_dout (mux2 middle_valid middle_dout fifo.q)
+  ; empty
+  }
+;;
+
+let create_showahead_from_classic
+      ?nearly_empty
+      ?nearly_full
+      ?overflow_check
+      ?reset
+      ?underflow_check
+      ()
+      ~capacity ~clock ~clear ~wr ~d ~rd =
+  let spec = Reg_spec.create ~clock:clock ~clear:clear () in
+  let fifo_rd_en = wire 1 in
+  let fifo =
+    create ~showahead:false ?nearly_empty ?nearly_full ?overflow_check ?reset ?underflow_check ()
+      ~capacity ~clock ~clear ~wr ~d ~rd:fifo_rd_en
+  in
+  let dout_valid = reg spec ~enable:(fifo_rd_en |: rd) fifo_rd_en in
+  let empty = ~:dout_valid in
+  fifo_rd_en <== (~:(fifo.empty) &: (~:dout_valid |: rd));
+  { fifo with empty }
+;;
+
+let create_showahead_with_extra_reg
+      ?nearly_empty
+      ?nearly_full
+      ?overflow_check
+      ?reset
+      ?underflow_check
+      ()
+      ~capacity ~clock ~clear ~wr ~d ~rd =
+  let spec = Reg_spec.create ~clock:clock ~clear:clear () in
+  let fifo_rd_en = wire 1 in
+  let fifo =
+    create ~showahead:false ?nearly_empty ?nearly_full ?overflow_check ?reset ?underflow_check ()
+      ~capacity ~clock ~clear ~wr ~d ~rd:fifo_rd_en
+  in
+  let fifo_valid = wire 1 in
+  let middle_valid = wire 1 in
+  let dout_valid = wire 1 in
+  let will_update_dout = (middle_valid |: fifo_valid) &: (rd |: ~:dout_valid) in
+  let will_update_middle = fifo_valid &: (middle_valid ==: will_update_dout) in
+  let empty = ~:dout_valid in
+  let middle_dout = reg spec ~enable:will_update_middle fifo.q in
+  let dout = reg spec ~enable:will_update_dout (mux2 middle_valid middle_dout fifo.q) in
+  fifo_rd_en <== ((~:(fifo.empty)) &: ~:(middle_valid &: dout_valid &: fifo_valid));
+  fifo_valid
+  <== reg spec ~enable:(fifo_rd_en |: will_update_middle |: will_update_dout) fifo_rd_en;
+  middle_valid
+  <== reg spec ~enable:(will_update_middle |: will_update_dout) will_update_middle;
+  dout_valid <== reg spec ~enable:(will_update_dout |: rd) will_update_dout;
+  { fifo with q = dout; empty }
+;;
--- src/fifo.mli
+++ src/fifo.mli
@@ -11,34 +11,11 @@ type t =
   ; used         : Signal.t }
 [@@deriving sexp_of]
 
-(** [create ~clk ~clr ~wr ~d ~rd capacity] builds a FIFO with [capacity] elements which is
-    written with [d] when [wr] is high and read when [rd] is high.
-
-    The default reset configuration is to use a synchronous [clr] signal.  An asynchronous
-    [rst] may be optionally provided.  One of [clr] or [rst] must be non-empty.
-
-    Optional overflow and underflow checking may be used.  Data will not be written(/read)
-    when the fifo is [full](/[empty]) regardles or the [wr]/([rd]) signals.
-
-    [nearly_emtpy] and [nearly_full] may be programmed to go high when the fifo is nearing
-    an underflow or overflow state.
-
-    The [showahead] mode changes the read behaviour of the FIFO.  When showahead is
-    [false] read data is available 1 cycle after [rd] is high.  With showahead [true] the
-    data is available on the same cycle as [rd] is high.  To support [showahead] behaviour
-    the timing of the [full]/[empty] flag also changes (although they still correctly
-    indicate when it is safe to read or write to the FIFO).  [showahead] mode has some
-    extra cost in terms of extra logic and reduced frequency.
-
-    Note; [showahead] is sometimes referred to as "first word fall through".
-
-    The [used] output indicates the number of elements currently in the FIFO. *)
-val create
-  :  ?nearly_empty    : int       (** default is [1] **)
+type create_fifo
+  =  ?nearly_empty    : int       (** default is [1] **)
   -> ?nearly_full     : int       (** default is [depth-1] **)
   -> ?overflow_check  : bool      (** default is [true] *)
   -> ?reset           : Signal.t  (** default is [empty] **)
-  -> ?showahead       : bool      (** default is [false] **)
   -> ?underflow_check : bool      (** default is [true] *)
   -> unit
   -> capacity : int
@@ -49,3 +26,48 @@ val create
   -> rd    : Signal.t
   -> t
 
+(** {4 Base RTL FIFO} *)
+
+(** [create ~clock ~clear ~wr ~d ~rd capacity] builds a FIFO with [capacity] elements
+    which is written with [d] when [wr] is high and read when [rd] is high.
+
+    The default reset configuration is to use a synchronous [clr] signal.  An asynchronous
+    [rst] may be optionally provided.  One of [clr] or [rst] must be non-empty.
+
+    Optional overflow and underflow checking may be used.  Data will not be written(/read)
+    when the fifo is [full](/[empty]) regardles or the [wr]/([rd]) signals.
+
+    [nearly_emtpy] and [nearly_full] may be programmed to go high when the fifo is nearing
+    an underflow or overflow state.
+
+    The [showahead] mode changes the read behaviour of the FIFO. When showahead is [false]
+    read data is available 1 cycle after [rd] is high. With showahead [true] the data is
+    available on the same cycle as [rd] is high. To support [showahead] behaviour the
+    timing of the [full]/[empty] flag also changes (although they still correctly indicate
+    when it is safe to read or write to the FIFO). [showahead] mode has some extra cost in
+    terms of extra logic. The implementation ensures the output is registered and timing
+    performance is good - nearly as fast as the underlying RAM allows..
+
+    Note; [showahead] is sometimes referred to as "first word fall through".
+
+    The [used] output indicates the number of elements currently in the FIFO. *)
+val create
+  :  ?showahead:bool (** default is [false] *)
+  -> create_fifo
+
+
+(** {3 Derived FIFO architectures.} *)
+
+(** Adds an extra output register to the non-showahead fifo. This delays the output, but
+    ensures there is no logic data on the fifo output. Adds an extra cycle of latency (2
+    cycles from write to empty low).*)
+val create_classic_with_extra_reg : create_fifo
+
+(** Constructs a showahead fifo from a non-showahead fifo. Only modifies the control
+    flags. Has 2 cycles of latency. *)
+val create_showahead_from_classic : create_fifo
+
+(** Constructs a fifo similarly to [create_showahead_from_classic] and ensures the output
+    data is registered. Has 3 cycles of latency, but is slightly faster than [create
+    ~showahead:true] - it seems to only be limited by the underlying RAM frequency. *)
+val create_showahead_with_extra_reg : create_fifo
